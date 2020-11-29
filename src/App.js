a2fb89ca914b267cdc0037353e48f2cf5e987e50
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-11-29 22:22:46 +0700

    Update authentication mechanism

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Login } from './pages/Login';
 import Main from './pages/Main';
-import { NotFound } from './pages/NotFound';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ProtectedRoute, AuthRoute } from './utils/CostomRoute';
+import auth from './utils/Auth';
 
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <ProtectedRoute exact path='/' component={Main} />
-        <AuthRoute path='/login' component={Login} />
-        <Route path='*' component={NotFound} />
-      </Switch>
-    </Router>
-  );
+  if (auth.isAuthenticated()) {
+    return <Main />;
+  } else {
+    return <Login />;
+  }
 }
 
 export default App;
